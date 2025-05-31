8183e4abf2071d7a1c13ef6f064be2d0b73f6912
Anand raja <meetanandns@gmail.com>
2025-05-31 17:09:38 +0000

    copy base64 feature added

--- src/app/components/image-detail/image-detail.ts
+++ src/app/components/image-detail/image-detail.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DatePipe, NgIf } from '@angular/common';
 
@@ -11,8 +11,9 @@ import { ImageItem } from '../../models/image.model';
   templateUrl: './image-detail.html',
   styleUrls: ['./image-detail.scss'],
 })
-export class ImageDetail implements OnInit {
+export class ImageDetail implements OnInit, OnDestroy {
   image?: ImageItem;
+  objectUrl?: string;
 
   private route = inject(ActivatedRoute);
   private imageService = inject(ImageService);
@@ -22,8 +23,52 @@ export class ImageDetail implements OnInit {
       const id = params.get('id');
       if (id) {
         this.image = this.imageService.getImageById(id);
+        if (this.image) {
+          this.createObjectUrl();
+        }
       }
     });
+  }
+
+  createObjectUrl(): void {
+    if (!this.image) return;
+
+    // Convert base64 to Blob
+    const base64Response = fetch(this.image.base64);
+    base64Response
+      .then(res => res.blob())
+      .then(blob => {
+        // Create object URL from Blob
+        this.objectUrl = URL.createObjectURL(blob);
+      });
+  }
+
+  openInNewTab(): void {
+    if (this.objectUrl) {
+      window.open(this.objectUrl, '_blank');
+    }
+  }
+
+  copyObjectUrl(): void {
+    if (this.objectUrl) {
+      navigator.clipboard
+        .writeText(this.objectUrl)
+        .then(() => alert('URL copied to clipboard!'))
+        .catch(err => console.error('Could not copy URL: ', err));
+    }
+  }
+
+  copyBase64(): void {
+    if (this.image?.base64) {
+      navigator.clipboard
+        .writeText(this.image.base64)
+        .then(() => {
+          alert('Base64 data copied to clipboard!');
+        })
+        .catch(err => {
+          console.error('Could not copy Base64 data: ', err);
+        });
+    }
   }
 
   downloadImage(): void {
@@ -35,5 +80,12 @@ export class ImageDetail implements OnInit {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+  }
+
+  ngOnDestroy(): void {
+    // Clean up the object URL to avoid memory leaks
+    if (this.objectUrl) {
+      URL.revokeObjectURL(this.objectUrl);
+    }
   }
 }
